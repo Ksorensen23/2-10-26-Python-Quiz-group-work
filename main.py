a893bcc8ed84fbbf65ebb121a96ee0f172e61b66
Flaes2 <FlaesPlays@gmail.com>
2026-02-10 13:50:38 -0500

    Update main.py (broken for now)
    saving this work, but it is actively broken. don't use it

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def load_questions():
 # 2. THE REFEREE'S MAIN LOOP
 def play_game():
     questions = load_questions()
-    score = 0
+    p1Score = 0
+    p2Score = 0
     
     print("--- WELCOME TO THE IT QUIZ BATTLE ---")
     
@@ -17,15 +18,23 @@ def play_game():
         for option in q['options']:
             print(option)
             
-        guess = input("Your Answer (A/B/C/D): ").upper()
+        guess = input("\nYour Answer (A/B/C/D): ").upper()
         
         if guess == q['answer']:
-            print("Correct! +10 points.")
-            score += 10
+            
+            if q % 2 == 0:
+                print("Correct! Player 1 +10 points.")
+                p1Score += 10
+
+            elif q % 2 != 0:
+                print("Correct! Player 2 +10 points.")
+                p2Score += 10
+                
         else:
             print(f"Wrong! The answer was {q['answer']}.")
             
-    print(f"\nGame Over! Final Score: {score}")
+    print(f"\nGame Over! Final Scores:\n\n Player 1 - {p1Score}\n\nPlayer 2 - {p2Score}")
 
 if __name__ == "__main__":
     play_game()
+
