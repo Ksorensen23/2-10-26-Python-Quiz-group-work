0b34c168cd7a875a4085f67eef4d48b587a11568
Flaes2 <FlaesPlays@gmail.com>
2026-02-11 12:55:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,31 +20,52 @@ def save_score(name, score):
 # 2. THE REFEREE'S MAIN LOOP
 def play_game():
     questions = load_questions()
-    score = 0
-
+    p1Score = 0
+    p2Score = 0
+    player = 1
+    
     name = input("Enter Your name\n")
     print("Welcome " + name)
     print("\n--- WELCOME TO THE IT QUIZ BATTLE ---")
     
     for q in questions:
+        
         print("\n" + q['question'])
         for option in q['options']:
             print(option)
             
-        guess = input("Your Answer (A/B/C/D): ").upper()
+        guess = input("\nPlayer" + f" {player} " + "Answer (A/B/C/D): ").upper()
         
         if guess == q['answer']:
-            print(Fore.GREEN + "Correct! +10 points.")
-            score += 10
-            print("\n Your score is:", score)
+
+            # if player one, add to their score
+            if player == 1:
+                print(Fore.GREEN + "Correct! Player 1 +10 points.")
+                p1Score += 10
+                print("\n Player 1 score is:", p1Score)
+                
+            # if player two, add to their score
+            elif player == 2:
+                print(Fore.GREEN + "Correct! Player 2 +10 points.")
+                p2Score += 10
+                print("\n Player 2 score is:", p2Score)
+                
         else:
             print(Fore.RED + f"Wrong! The answer was {q['answer']}.")
             
+        # This changes which player is active
+        #   it must be put here otherwise it would need to be
+        #   duplicated in the correct and incorrect logic conditions
+        if player == 2:
+            player = 1
+        elif player == 1:
+            player = 2
             
-    print(f"\nGame Over! Final Score: {score}")
+    print(f"\nGame Over! Final Scores: \n\nPlayer 1 - {p1Score} Points\nPlayer 2 - {p2Score} Points")
     save_score(name, score)
 
 
     
 if __name__ == "__main__":
     play_game()
+
