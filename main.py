72c88e90c0dea273ed6d3641fa82468569ec17a8
Clixked <cgirotti.jpkeefe@gmail.com>
2026-02-11 11:46:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import colorama import Fore
 
 
 # 1. THE LIBRARIAN'S FUNCTION
@@ -32,11 +33,11 @@ def play_game():
         guess = input("Your Answer (A/B/C/D): ").upper()
         
         if guess == q['answer']:
-            print("Correct! +10 points.")
+            print(Fore.GREEN + "Correct! +10 points.")
             score += 10
             print("\n Your score is:", score)
         else:
-            print(f"Wrong! The answer was {q['answer']}.")
+            print(Fore.RED + f"Wrong! The answer was {q['answer']}.")
             
             
     print(f"\nGame Over! Final Score: {score}")
