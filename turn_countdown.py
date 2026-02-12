e2dd862ec4d47a79016e0e2c059eb98de1fee92f
strange-Dyed <asiswick2028@jpkeefehs.org>
2026-02-12 11:57:04 -0500

    updated timer length and display, not very important

--- turn_countdown.py
+++ turn_countdown.py
@@ -2,16 +2,16 @@ import time;
 
 start = time.time();                        #declares the start of the timer
 
-for i in range(11):                         #sets the timer length, default at 10 seconds
+for i in range(10):                         #sets the timer length, default at 10 seconds
     time.sleep(1);                          #sets the interval delay to 1 second
-    print("time left:", abs(i - 10));       #displays a message every second, abs() displays the correct time left
+    print("time left:", abs(i - 9));       #displays a message every second, abs() displays the correct time left
 
 end = time.time();                          #declares the end of the timer
 
 length = end - start;                       #finds the total elapsed time
 length = int(length);                       #rounds the time to the nearest second
 
-print("your time is up!", length - 1, "it took seconds");    #displays total elapsed time
+print("it took", length - 1, "seconds");    #displays total elapsed time
 
 #IMPORTANT, to be merged with the randomization function in the complete logic file
 
@@ -25,6 +25,6 @@ for i in range(11):
     print("time left:", i);
 '''
 
-
 #>==================================================
+
 
